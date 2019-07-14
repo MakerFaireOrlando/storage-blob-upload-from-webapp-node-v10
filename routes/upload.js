61a21d1f97110f68176eef3b5d7c1758c78d823b
Jeff Driscoll <jeffdriscoll@MacBook-Pro.localdomain>
2019-07-14 09:35:11 -0400

    return blobURL in addition to message

--- routes/upload.js
+++ routes/upload.js
@@ -53,7 +53,10 @@ router.post('/', uploadStrategy, async (req, res) => {
     await uploadStreamToBlockBlob(aborter, stream,
       blockBlobURL, uploadOptions.bufferSize, uploadOptions.maxBuffers);
 
-    res.render('success', { message: 'File uploaded to Azure Blob storage.' });   
+    res.render('success',
+                { message: 'File uploaded to Azure Blob storage.' },
+                { bloburl: 'blob url goes here'}
+              );   
 
   } catch (err) {
 
